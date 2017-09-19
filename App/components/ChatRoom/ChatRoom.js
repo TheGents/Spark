b2a75e934bf2470ceff46d065e9dcf687391c800
cy <cy@Christophers-MacBook-Pro.local>
2017-09-19 17:01:21 -0500

    chatroom

--- App/components/ChatRoom/ChatRoom.js
+++ App/components/ChatRoom/ChatRoom.js
@@ -1,28 +1,75 @@
-
-import React, { Component } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
-
+import React, {Component} from 'react';
+import {View, Text, StyleSheet, TouchableOpacity, Image} from 'react-native';
+import {GiftedChat} from 'react-native-gifted-chat';
 
 class ChatRoom extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      messages: []
+    };
+  }
+
+  componentWillMount() {
+    this.setState({
+      // messages: [
+      //   {
+      //     _id: 1,
+      //     text: 'This is grey while messager is blue',
+      //     createdAt: new Date(),
+      //     user: {
+      //       _id: 2,
+      //       name: 'React Native',
+      //       avatar: 'https://facebook.github.io/react/img/logo_og.png'
+      //     }
+      //   }
+      // ]
+    });
+  }
+
+  onSend(messages = []) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages)
+    }));
+  }
+
   render() {
     return (
-      <View>
+      <View style={styles.container}>
         <View style={styles.nav}>
-          <TouchableOpacity 
+          <TouchableOpacity
             onPress={() => {
-            this.props.navigation.navigate('Messages');
-            }}
-          >
-          <Image source ={require('../images/suit.png')} name="ios-person" color ="#888" size={25} style={{width:30, height:30, margin:10}} />
+            this
+              .props
+              .navigation
+              .navigate('Messages');
+          }}>
+            <Image
+              source={require('../images/suit.png')}
+              name="ios-person"
+              color="#888"
+              size={25}
+              style={{
+              width: 30,
+              height: 30,
+              margin: 10
+            }}/>
           </TouchableOpacity>
-          <Image source ={require('../images/logo.png')} resizeMode = "contain" style={{width:100, height:30}} />
+          <Image
+            source={require('../images/logo.png')}
+            resizeMode="contain"
+            style={{
+            width: 100,
+            height: 30
+          }}/>
         </View>
-        <View>
-          <Text>
-            Chatroom
-          </Text>
-        </View>
-     </View>  
+        <GiftedChat
+          messages={this.state.messages}
+          onSend={messages => this.onSend(messages)}
+          user={{
+          _id: 1
+        }}/>
+      </View>
     );
   }
 }
@@ -33,16 +80,15 @@ const styles = StyleSheet.create({
     padding: 10
   },
   nav: {
-    height:60,
-    flexDirection:'row',
-    paddingTop:10,
+    height: 60,
+    flexDirection: 'row',
+    paddingTop: 10,
     justifyContent: 'space-between',
-    alignItems:'center',
+    alignItems: 'center',
     backgroundColor: '#fff',
-    borderBottomWidth:1,
-    borderColor:'rgba(0,0,0,0.1)'
-  },
+    borderBottomWidth: 1,
+    borderColor: 'rgba(0,0,0,0.1)'
+  }
 });
 
 export default ChatRoom;
-
